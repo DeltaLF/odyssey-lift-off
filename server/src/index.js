bd4971adebf922e090f94f5b78012e1533ac679f
Patrick <patricktsaikl@gmail.com>
2023-02-14 13:19:58 +0000

    setup apilloserver and mock data

--- server/src/index.js
+++ server/src/index.js
@@ -1,1 +1,11 @@
-//TODO
+const { ApolloServer } = require("apollo-server");
+const typeDefs = require("./schema");
+const { mocks } = require("./mocks/index");
+
+const server = new ApolloServer({ typeDefs: typeDefs, mocks: mocks });
+
+server.listen().then(() => {
+  console.log(
+    "server is running on port 4000m query at http://localhost:4000 "
+  );
+});
---
+++ server/src/mocks/index.js
@@ -0,0 +1,22 @@
+const mocks = {
+  Query: () => ({
+    tracksForHome: () => [...new Array(6)],
+  }),
+  Track: () => ({
+    id: () => "track_01",
+    title: () => "This is track one",
+    author: () => {
+      return {
+        name: "Grumpy Cat",
+        photo:
+          "https://res.cloudinary.com/dety84pbu/image/upload/v1606816219/kitty-veyron-sm_mctf3c.jpg",
+      };
+    },
+    thumbnail: () =>
+      "https://res.cloudinary.com/dety84pbu/image/upload/v1598465568/nebula_cat_djkt9r.jpg",
+    length: () => 300,
+    moduleCount: () => 10,
+  }),
+};
+
+module.exports = { mocks };
