70ef3133d345a56681c0c0c1fe75dd28a8d538a8
Patrick <patricktsaikl@gmail.com>
2023-02-14 14:01:27 +0000

    setup apollo client at frontend side

--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import GlobalStyles from './styles';
-import Pages from './pages';
+import React from "react";
+import ReactDOM from "react-dom";
+import GlobalStyles from "./styles";
+import Pages from "./pages";
+// apollo
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
+
+const apolloClient = new ApolloClient({
+  uri: "http://localhost:4000",
+  cache: new InMemoryCache(),
+});
 
 ReactDOM.render(
   <React.StrictMode>
-    <GlobalStyles />
-    <Pages />
+    <ApolloProvider client={apolloClient}>
+      <GlobalStyles />
+      <Pages />
+    </ApolloProvider>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
